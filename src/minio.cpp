6699d6d78ab99a4e06633c326a4d8a10fce2c9b6
丛继晔 <congtheking@outlook.com>
2020-11-13 03:18:40 +0000

    add src projects

---
+++ src/minio.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include <aws/core/Aws.h>
+#include <aws/s3/S3Client.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
+#include <aws/s3/model/CreateBucketRequest.h>
+#include <aws/s3/model/PutObjectRequest.h>
+#include <aws/s3/model/GetObjectRequest.h>
+#include <aws/s3/model/HeadBucketRequest.h>
+#include <sys/stat.h>
+#include <fstream>
+
+#include "minio.h"
+
+namespace Minio
+{
+    Minio::Minio(string address, string access_key, string secret_key, bool secure) : kAddress(address),
+                                                                                      kAccessKey(access_key),
+                                                                                      kSecretKey(secret_key),
+                                                                                      kSecure(secure)
+    {
+        options_ = new Aws::SDKOptions;
+        auto options = reinterpret_cast<Aws::SDKOptions *>(options_);
+        Aws::InitAPI(*options);
+
+        config_ = new Aws::Client::ClientConfiguration;
+        auto config = reinterpret_cast<Aws::Client::ClientConfiguration *>(config_);
+        config->endpointOverride = kAddress;
+
+        if (kSecure)
+        {
+            config->scheme = Aws::Http::Scheme::HTTPS;
+            config->verifySSL = true;
+        }
+        else
+        {
+            config->scheme = Aws::Http::Scheme::HTTP;
+            config->verifySSL = false;
+        }
+
+        s3_client_ = new Aws::S3::S3Client(Aws::Auth::AWSCredentials(kAccessKey, kSecretKey), *config,
+                                           Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
+    }
+
+    Minio::~Minio()
+    {
+        auto options = reinterpret_cast<Aws::SDKOptions *>(options_);
+        Aws::ShutdownAPI(*options);
+
+        delete reinterpret_cast<Aws::SDKOptions *>(options_);
+        delete reinterpret_cast<Aws::Client::ClientConfiguration *>(config_);
+        delete reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+    }
+
+    bool Minio::GetObject(string from_bucket, string object_key, string file_path)
+    {
+        auto s3_client = reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+        Aws::S3::Model::GetObjectRequest request;
+        request.SetBucket(from_bucket);
+        request.SetKey(object_key);
+
+        auto outcome = s3_client->GetObject(request);
+
+        if (outcome.IsSuccess())
+        {
+            Aws::OFStream local_file;
+            local_file.open(file_path, std::ios::out | std::ios::binary);
+            local_file << outcome.GetResultWithOwnership().GetBody().rdbuf();
+            return true;
+        }
+        else
+        {
+            auto err = outcome.GetError();
+            std::cerr << "Error: GetObject: " << err.GetExceptionName() << ": " << err.GetMessage() << std::endl;
+            return false;
+        }
+    }
+
+    bool Minio::PutObject(string bucket_name, string file_path, string file_name)
+    {
+        struct stat buffer;
+
+        if (stat(file_path.c_str(), &buffer) == -1)
+        {
+            std::cout << "Error PutObject: File " << file_name << " does not exist"
+                      << "\n";
+            return false;
+        }
+        auto s3_client = reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+        Aws::S3::Model::PutObjectRequest request;
+        request.SetBucket(bucket_name);
+        request.SetKey(file_name);
+
+        std::shared_ptr<Aws::IOStream> input_data = Aws::MakeShared<Aws::FStream>("sSampleAllocationtTag", file_path.c_str(), std::ios_base::in | std::ios_base::binary);
+        request.SetBody(input_data);
+        auto outcome = s3_client->PutObject(request);
+
+        if (outcome.IsSuccess())
+        {
+            std::cout << "Added object " << file_name << " to bucket ";
+            return true;
+        }
+        std::cout << "Error:putObjeact: " << outcome.GetError().GetMessage() << "\n";
+        return false;
+    }
+
+    bool Minio::MakeBucket(string bucket_name)
+    {
+        auto s3_client = reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+        Aws::S3::Model::CreateBucketRequest request;
+        request.SetBucket(bucket_name);
+
+        auto outcome = s3_client->CreateBucket(request);
+
+        if (!outcome.IsSuccess())
+        {
+            auto err = outcome.GetError();
+            std::cerr << "Error: CreateBucket: " << err.GetExceptionName() << ": " << err.GetMessage() << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    bool Minio::ListBuckets(vector<string> &buckets)
+    {
+        auto s3_client = reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+        auto outcome = s3_client->ListBuckets();
+        vector<string>().swap(buckets);
+
+        if (outcome.IsSuccess())
+        {
+            auto resp = outcome.GetResult().GetBuckets();
+
+            for (auto &bucket : resp)
+            {
+                buckets.push_back(bucket.GetName());
+            }
+            return true;
+        }
+        else
+        {
+            std::cerr << "Error: ListBuckets: " << outcome.GetError().GetMessage() << std::endl;
+            return false;
+        }
+    }
+
+    bool Minio::BucketExists(string bucket_name)
+    {
+        auto s3_client = reinterpret_cast<Aws::S3::S3Client *>(s3_client_);
+        Aws::S3::Model::HeadBucketRequest request;
+        request.SetBucket(bucket_name);
+
+        auto outcome = s3_client->HeadBucket(request);
+        if (!outcome.IsSuccess())
+        {
+            auto err = outcome.GetError();
+            std::cerr << "Error: BucketExists: "
+                      << err.GetExceptionName() << "\n"
+                      << err.GetMessage() << std::endl;
+            return false;
+        }
+        return true;
+    }
+} // namespace Minio
---
+++ src/minio.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <string>
+#include <vector>
+namespace Minio
+{
+    using namespace std;
+    using ClientConfiguration = void *;
+    using SDKOptions = void *;
+    using S3Client = void *;
+
+    class Minio
+    {
+    public:
+        /**
+         * @brief Minio 构造函数
+         * @param  address          Minio 服务所在地址，可以使用 IP:Port 的形式
+         * @param  access_key       Minio 登录使用的 access_key
+         * @param  secret_key       Minio 登录使用的 secret_key
+         */
+        Minio(string address, string access_key, string secret_key, bool secure = false);
+        ~Minio();
+
+        /**
+         * @brief 从 Minio 中下载文件
+         * @param  from_bucket      文件所在的桶名
+         * @param  object_key       文件的路径
+         * @param  file_path        文件保存的路径
+         * @return 如何下载成功返回 true 否则会打印失败信息并返回 false
+         */
+        bool GetObject(string from_bucket, string object_key, string file_path);
+
+        /**
+         * @brief upload data from a file to an object
+         * @param bucket_name       name of bucket
+         * @param file_path         absolute path of file
+         * @param file_name         name of file
+         * @return if file upload is success returned true, if file upload is error returned false
+         */
+        bool PutObject(string bucket_name, string file_path,string file_name);
+
+        /**
+         * @brief 创建一个存储桶
+         * @param  bucket_name      存储桶名称
+         * @return 存储桶创建成功返回 true 否则打印识别信息并返回 false
+         */
+        bool MakeBucket(string bucket_name);
+
+        /**
+         * @brief 显示所有的存储桶
+         * @param  buckets          返回的所有桶信息
+         * @return true 
+         */
+        bool ListBuckets(vector<string>& buckets);
+
+        /**
+         * @brief 判断存储桶是否存在
+         * @param  bucket_name      存储桶名称
+         * @return 如果存储桶存在则返回 true 否则返回 false
+         */
+        bool BucketExists(string bucket_name);
+
+
+    private:
+        const string kAddress;
+        const string kAccessKey;
+        const string kSecretKey;
+        const bool kSecure;
+
+        SDKOptions options_;
+        ClientConfiguration config_;
+        S3Client s3_client_;
+    };
+} // namespace Minio

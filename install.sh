684f28913af19dc1cbcbf471ccf18f962d683efa
丛继晔 <congtheking@outlook.com>
2021-05-07 09:11:18 +0000

    fix: 修改安装脚本
    * 安装脚本参数将 CMAKE_BUILD 修改为 CMAKE_BUILD_TYPE
    
    * 增加默认安装版本为 Release

--- install.sh
+++ install.sh
@@ -25,22 +25,24 @@ fi
 cd ${source_path}
 git submodule update --init --recursive
 
+build_type=Release
 if [[ $# -lt 1 ]];then
     echo "Build Type: Release"
 elif [[ $1 != "Debug" && $1 != "Release" ]];then
     echo "Error Type: ${1}"
     echo "Legal parameters is Debug or Release"
+else
+    build_type=${1}
 fi
 
 cd aws-sdk-cpp
-build_type=$1
 mkdir build && cd build
-cmake -DBUILD_ONLY="s3" -DCMAKE_BUILD=${build_type} -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$(pwd)/install ..
+cmake -DBUILD_ONLY="s3" -DCMAKE_BUILD_TYPE=${build_type} -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=$(pwd)/install ..
 make && make install
 
 cd ../..
 mkdir build && cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=${build_type} ..
 make && make install
 
 echo "All Done."
